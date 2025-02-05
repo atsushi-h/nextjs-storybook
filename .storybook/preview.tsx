da091445d6ec7428d89d33d2e7ccc233165875bf
atsushi-h <atsushi.1213eng@gmail.com>
2025-02-05 20:59:51 +0900

    msw init

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { makeQueryClient } from '../src/app/provider';
 import '../src/app/globals.css';
+
+// Initialize MSW
+initialize();
 
 const queryClient = makeQueryClient();
 
@@ -27,6 +31,8 @@ const preview: Preview = {
       </QueryClientProvider>
     ),
   ],
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader],
 };
 
 export default preview;
